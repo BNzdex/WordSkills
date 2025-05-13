7cf91895be7713622dde34b05a203093470f187f
BNzdex <bernardolocatelli28@gmail.com>
2025-05-13 09:57:02 -0300

    Update ativ05.cs

--- ativ05.cs
+++ ativ05.cs
@@ -14,11 +14,11 @@ class Program {
             Console.WriteLine(n1);
             n1 += 1;
         }
+        Console.WriteLine("\n========== Segunda parte da atividade ==========\n");
         int soma = 0;
         for (int i = 1; i < 101; i++){
             soma += i;
         }
-        Console.WriteLine("\n========== Segunda parte da atividade ==========\n");
         Console.WriteLine($"A soma dos números de 1 a 100 é {soma}");
         
     }     
