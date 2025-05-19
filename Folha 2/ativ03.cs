660e416519039ecfa9d53b553ef47c2f4d6fc3ad
BNzdex <bernardolocatelli28@gmail.com>
2025-05-19 08:21:31 -0300

    Add files via upload

---
+++ Folha 2/ativ03.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+class AlunoTXT
+{
+    static void Main()
+    {
+        string arquivo = File.ReadAllText ("aluno.txt");
+        Console.WriteLine ("Alunos cadastrados: ");
+        Console.WriteLine (arquivo);
+        
+    }
+}
