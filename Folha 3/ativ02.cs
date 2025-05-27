e55cb4ae06e277955417572f317b99fdbd84dab6
BNzdex <bernardolocatelli28@gmail.com>
2025-05-27 09:25:24 -0300

    Create ativ02.cs

---
+++ Folha 3/ativ02.cs
@@ -0,0 +1,50 @@
+using System;
+
+class Aluno
+{
+    static void Main()
+    {
+        Console.WriteLine("*********************************");
+        Console.WriteLine("Bem-vindo ao atendimento virtual!");
+        Console.WriteLine("*********************************");
+
+        while (true)
+        {
+            Console.WriteLine("Digite uma opção: ");
+            Console.WriteLine("1- Informações sobre produtos");
+            Console.WriteLine("2- Suporte técnico");
+            Console.WriteLine("3- Falar com atendente");
+            Console.WriteLine("0- Encerrar conversa");
+            Console.WriteLine("O que deseja fazer ? ");
+            string opcao = Console.ReadLine();
+
+            if (opcao == "1" || opcao.ToUpper() == "INFORMAÇÕES SOBRE PRODUTOS")
+            {
+                Console.WriteLine("Temos os seguites produtos: Notebook, Monitor, Teclado");
+            }
+            else if (opcao == "2" || opcao.ToUpper() == "SUPORTE TÉCNICO")
+            {
+                Console.WriteLine("Para suporte técnico, acesse: mytech.com");
+            }
+            else if (opcao == "3" || opcao.ToUpper() == "FALAR COM ATENDENTE")
+            {
+                Console.WriteLine("Um atendente estará com você em breve");
+            }
+            else if (opcao == "0" || opcao.ToUpper() == "ENCERRAR CONVERSA")
+            {
+                Console.WriteLine("Até mais!");
+                break;
+            }
+            else if (opcao.ToUpper() == "OLÁ")
+                Console.WriteLine("Olá");
+
+            else if (opcao.ToUpper() == "OLÁ, BOM DIA" || opcao.ToUpper() == "OLÁ BOM DIA" || opcao.ToUpper() == "BOM DIA")
+                Console.WriteLine("Olá, bom dia");
+
+            else if (opcao.ToUpper() == "OLÁ, BOA TARDE" || opcao.ToUpper() == "OLÁ BOA TARDE" || opcao.ToUpper() == "BOA TARDE")
+                Console.WriteLine("Olá, boa tarde");
+            else
+                Console.WriteLine("Opção inválida, tente novamente");
+        }
+    }
+}
