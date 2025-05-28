308d034dee44eca0afb7df75481742b2e8243078
BNzdex <bernardolocatelli28@gmail.com>
2025-05-28 10:31:11 -0300

    Add files via upload

---
+++ Folha 3/ativ2/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Class2
+{
+    public static void ExibirMenu()
+    {
+        Console.WriteLine("Digite uma opção: ");
+        Console.WriteLine("1- Informações sobre produtos");
+        Console.WriteLine("2- Suporte técnico");
+        Console.WriteLine("3- Falar com atendente");
+        Console.WriteLine("0- Encerrar conversa");
+        Console.WriteLine("O que deseja fazer ? ");
+    }
+
+    public static bool ProcessarOpcao(string opcao)
+    {
+        if (opcao == "1" || opcao.ToUpper() == "INFORMAÇÕES SOBRE PRODUTOS")
+        {
+            Console.WriteLine("Temos os seguites produtos: Notebook, Monitor, Teclado");
+        }
+        else if (opcao == "2" || opcao.ToUpper() == "SUPORTE TÉCNICO")
+        {
+            Console.WriteLine("Para suporte técnico, acesse: mytech.com");
+        }
+        else if (opcao == "3" || opcao.ToUpper() == "FALAR COM ATENDENTE")
+        {
+            Console.WriteLine("Um atendente estará com você em breve");
+        }
+        else if (opcao == "0" || opcao.ToUpper() == "ENCERRAR CONVERSA")
+        {
+            Console.WriteLine("Até mais!");
+            return true;
+        }
+        else if (opcao.ToUpper() == "OLÁ")
+        {
+            Console.WriteLine("Olá");
+        }
+        else if (opcao.ToUpper() == "OLÁ, BOM DIA" || opcao.ToUpper() == "OLÁ BOM DIA" || opcao.ToUpper() == "BOM DIA")
+        {
+            Console.WriteLine("Olá, bom dia");
+        }
+        else if (opcao.ToUpper() == "OLÁ, BOA TARDE" || opcao.ToUpper() == "OLÁ BOA TARDE" || opcao.ToUpper() == "BOA TARDE")
+        {
+            Console.WriteLine("Olá, boa tarde");
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida, tente novamente");
+        }
+
+        return false;
+    }
+}
+
---
+++ Folha 3/ativ2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Aluno
+{
+    static void Main()
+    {
+        Console.WriteLine("*********************************");
+        Console.WriteLine("Bem-vindo ao atendimento virtual!");
+        Console.WriteLine("*********************************");
+
+        while (true)
+        {
+            Class2.ExibirMenu();
+            string opcao = Console.ReadLine();
+
+            if (Class2.ProcessarOpcao(opcao))
+                break;
+        }
+    }
+
+}
