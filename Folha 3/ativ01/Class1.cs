cf90a3e77742b72bae904d25517e5f3a59f9c848
BNzdex <bernardolocatelli28@gmail.com>
2025-05-29 10:06:39 -0300

    Update Class1.cs

--- Folha 3/ativ01/Class1.cs
+++ Folha 3/ativ01/Class1.cs
@@ -1,68 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ativ01
+public class Class1
 {
-    public class Class1
+    public static List<string> txt = new List<string>();
+    public static (List<string> nomes, List<int> faltas, List<int> notas, int quantidadeCadastros) CadastroAluno()
     {
-        public static (List<string> nomes, List<int> faltas, List<int> notas, int quantidadeCadastros) CadastroAluno()
+        var nomes = new List<string>();
+        var faltas = new List<int>();
+        var notas = new List<int>();
+
+        Console.WriteLine("Deseja cadastrar quantos alunos (de 1 a 5)? ");
+        int quantidadeCadastros = int.Parse(Console.ReadLine());
+        txt.Add($"Quantidade de cadastros: {quantidadeCadastros} alunos\n");
+
+        for (int i = 0; i < quantidadeCadastros; i++)
         {
-            var nomes = new List<string>();
-            var faltas = new List<int>();
-            var notas = new List<int>();
+            Console.Write($"Digite o nome do {i + 1}° Aluno: ");
+            nomes.Add(Console.ReadLine());
+            txt.Add($"Nome do {1 + i}° aluno: {nomes[i]}");
 
-            Console.WriteLine("Deseja cadastrar quantos alunos (de 1 a 5)? ");
-            int quantidadeCadastros = int.Parse(Console.ReadLine());
+            Console.Write($"Digite a quantidade de faltas do {i + 1}° Aluno: ");
+            faltas.Add(int.Parse(Console.ReadLine()));
+            txt.Add($"Faltas do {1 + i}° aluno: {faltas[i]}");
 
-            for (int i = 0; i < quantidadeCadastros; i++)
-            {
-                Console.Write($"Digite o nome do {i + 1}° Aluno: ");
-                nomes.Add(Console.ReadLine());
+            Console.Write($"Digite a nota final do {i + 1}° Aluno: ");
+            notas.Add(int.Parse(Console.ReadLine()));
+            txt.Add($"Nota do {1 + i}° aluno: {notas[i]}");
 
-                Console.Write($"Digite a quantidade de faltas do {i + 1}° Aluno: ");
-                faltas.Add(int.Parse(Console.ReadLine()));
-
-                Console.Write($"Digite a nota final do {i + 1}° Aluno: ");
-                notas.Add(int.Parse(Console.ReadLine()));
-
-                Console.WriteLine("Aluno cadastrado com sucesso!\n");
-            }
-
-            return (nomes, faltas, notas, quantidadeCadastros);
+            Console.WriteLine("Aluno cadastrado com sucesso!\n");
+            txt.Add("Aluno cadastrado com sucesso!\n");
         }
 
-        public static (List<string> situacaoNota, List<string> situacaoFaltas, int alunosReprovados) Relatorio(
-            List<string> nomes, List<int> notas, List<int> faltas, int quantidadeCadastros)
+        return (nomes, faltas, notas, quantidadeCadastros);
+    }
+
+    public static (List<string> situacaoNota, List<string> situacaoFaltas, int alunosReprovados) Relatorio(
+        List<string> nomes, List<int> notas, List<int> faltas, int quantidadeCadastros)
+    {
+        var situacaoNota = new List<string>();
+        var situacaoFaltas = new List<string>();
+        int alunosReprovados = 0;
+
+        for (int i = 0; i < quantidadeCadastros; i++)
         {
-            var situacaoNota = new List<string>();
-            var situacaoFaltas = new List<string>();
-            int alunosReprovados = 0;
+            Console.WriteLine($"{i + 1}° Aluno: {nomes[i]}");
 
-            for (int i = 0; i < quantidadeCadastros; i++)
-            {
-                Console.WriteLine($"{i + 1}° Aluno: {nomes[i]}");
+            if (notas[i] < 7)
+                situacaoNota.Add("Reprovado");
+            else
+                situacaoNota.Add("Aprovado");
 
-                if (notas[i] < 7)
-                    situacaoNota.Add("Reprovado");
-                else
-                    situacaoNota.Add("Aprovado");
+            if (faltas[i] < 5)
+                situacaoFaltas.Add("Regular");
+            else
+                situacaoFaltas.Add("Irregular");
 
-                if (faltas[i] < 5)
-                    situacaoFaltas.Add("Regular");
-                else
-                    situacaoFaltas.Add("Irregular");
+            Console.WriteLine($"Status Nota: {situacaoNota[i]}");
+            Console.WriteLine($"Status Faltas: {situacaoFaltas[i]}");
+            txt.Add($"\nStatus aluno {i + 1}\nNotas: {situacaoNota[i]}\nFaltas: {situacaoFaltas[i]}");
 
-                Console.WriteLine($"Status Nota: {situacaoNota[i]}");
-                Console.WriteLine($"Status Faltas: {situacaoFaltas[i]}");
-
-                if (situacaoNota[i] == "Reprovado" || situacaoFaltas[i] == "Irregular")
-                    alunosReprovados++;
-            }
-
-            return (situacaoNota, situacaoFaltas, alunosReprovados);
+            if (situacaoNota[i] == "Reprovado" || situacaoFaltas[i] == "Irregular")
+                alunosReprovados++;
         }
+        txt.Add($"\nTotal de alunos reprovados: {alunosReprovados}");
+        return (situacaoNota, situacaoFaltas, alunosReprovados);
     }
 }
+
