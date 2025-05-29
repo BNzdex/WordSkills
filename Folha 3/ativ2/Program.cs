6eab8853c3543702466ca9241fdd479dbf893dbf
BNzdex <bernardolocatelli28@gmail.com>
2025-05-29 10:50:17 -0300

    Update Program.cs

--- Folha 3/ativ2/Program.cs
+++ Folha 3/ativ2/Program.cs
@@ -16,7 +16,12 @@ class Program
             if (Class2.ProcessarOpcao(opcao))
                 break;
         }
-
+        
+        Console.WriteLine("De 1 a 10, como você avalia nosso atendimento? ");
+        int nota = int.Parse(Console.ReadLine());
+        Class2.historico.Add($"Avaliação: {nota}");
+        Console.WriteLine("Obrigado pela sua avaliação!!!\n");
+        
         File.WriteAllLines("chatlog.txt", Class2.historico);
         Console.WriteLine("Histórico da conversa salvo em 'chatlog.txt'.");
     }
