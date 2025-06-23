57d2fb9575a097e70271d2e1df6f9322be7215d2
BNzdex <bernardolocatelli28@gmail.com>
2025-06-23 09:49:58 -0300

    Add files via upload

---
+++ Livre/CadastroSolo/AnaliseSolo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+public class AnaliseSolo
+{
+    public string NomePropriedade { get; set; }
+    public string DataColeta { get; set; }
+    public double Ph { get; set; }
+    public double Fosforo { get; set; }
+    public double Potassio { get; set; }
+    public double Magnesio { get; set; }
+    public double Calcio { get; set; }
+}
+
+public static class CadastroSolo
+{
+    public static string caminhoArquivo = "AnaliseSolo.json";
+
+    public static List<AnaliseSolo> CarregarAnalises()
+    {
+        try
+        {
+            if (File.Exists(caminhoArquivo))
+            {
+                string json = File.ReadAllText(caminhoArquivo);
+                return JsonSerializer.Deserialize<List<AnaliseSolo>>(json) ?? new List<AnaliseSolo>();
+            }
+        }
+        catch 
+        {
+            Console.WriteLine("Erro ao carregar análises");
+        }
+
+        return new List<AnaliseSolo>();
+    }
+
+    public static void SalvarAnalises(List<AnaliseSolo> lista)
+    {
+        string json = JsonSerializer.Serialize(lista, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(caminhoArquivo, json);
+    }
+}
---
+++ Livre/CadastroSolo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\n***************************************");
+        Console.WriteLine("Sistema de Cadastro de Análises de Solo");
+        Console.WriteLine("***************************************");
+        List<AnaliseSolo> analises = CadastroSolo.CarregarAnalises();
+
+        while (true)
+        {
+            Console.WriteLine("O que deseja fazer ?");
+            Console.WriteLine("1- Cadastrar uma nova análise\n2- Listar todas análises cadastradas\n3- Sair do sistema");
+            Console.WriteLine("Escolha uma das opções: ");
+            string opcao = Console.ReadLine();
+
+            if (opcao.ToUpper().Contains("CADASTRAR") || opcao == "1")
+            {
+                Console.WriteLine("Digite o nome da propriedade: ");
+                string nome_propriedade = Console.ReadLine();
+                Console.WriteLine("Data de coleta (ex: 17/06/2025): ");
+                string data_coleta = Console.ReadLine();
+                Console.WriteLine("Digite o pH do solo: ");
+                double ph = double.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o Fósforo (P) do solo: ");
+                double p = double.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o Potássio (K) do solo: ");
+                double k = double.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o Magnésio (Mg) do solo: ");
+                double mg = double.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o Cálcio (Ca) do solo: ");
+                double ca = double.Parse(Console.ReadLine());
+
+                AnaliseSolo analiseSolo = new AnaliseSolo
+                {
+                    NomePropriedade = nome_propriedade,
+                    DataColeta = data_coleta,
+                    Ph = ph,
+                    Fosforo = p,
+                    Potassio = k,
+                    Magnesio = mg,
+                    Calcio = ca
+                };
+
+                analises.Add(analiseSolo);
+                CadastroSolo.SalvarAnalises(analises);
+
+                Console.WriteLine("Análise salva com sucesso!");
+            }
+            else if (opcao.ToUpper().Contains("LISTAR") || opcao == "2")
+            {
+                foreach(var analise in analises)
+                {
+                    Console.WriteLine($"\nNome da propriedade: {analise.NomePropriedade}");
+                    Console.WriteLine($"Data da coleta: {analise.DataColeta}");
+                    Console.WriteLine($"Potencial Hidrogeniônico (pH) do solo: {analise.Ph}");
+                    Console.WriteLine($"Fósforo (P) do solo: {analise.Fosforo}");
+                    Console.WriteLine($"Potássio (K) do solo: {analise.Potassio}");
+                    Console.WriteLine($"Magnésio (Mg) do solo: {analise.Magnesio}");
+                    Console.WriteLine($"Cálcio (Ca) do solo: {analise.Calcio}\n");
+                }
+            }
+            else if (opcao.ToUpper().Contains("SAIR") || opcao == "3")
+            {
+                Console.WriteLine("Até mais!!!");
+                break;
+            }
+            else
+                Console.WriteLine("Opção inválida! Tente novamente.");
+        }
+    }
+}
